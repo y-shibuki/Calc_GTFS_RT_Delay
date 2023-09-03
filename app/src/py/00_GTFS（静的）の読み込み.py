1c82881d8e6dd316893471ac9945a9325fa03952
Hinata Yoshida <12867630+y-shibuki@users.noreply.github.com>
2023-09-03 13:43:36 +0900

    GTFSをSQLに書き込む処理の追加

---
+++ app/src/py/00_GTFS（静的）の読み込み.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+
+import pandas as pd
+
+from app.utils.db import get_db_adapter
+
+db_adapter = get_db_adapter()
+
+
+for agency in ["関東自動車", "富山地鉄バス", "富山地鉄市内電車"]:
+    print(agency)
+
+    stop_times_df = pd.read_csv(f"./data/GTFS/{agency}/stop_times.txt")[
+        [
+            "trip_id",
+            "arrival_time",
+            "departure_time",
+            "stop_id",
+            "stop_sequence",
+            "stop_headsign",
+        ]
+    ]
+
+    stop_times_df["arrival_time"] = pd.to_datetime(
+        stop_times_df["arrival_time"], format="%H:%M:%S"
+    )
+    stop_times_df["departure_time"] = pd.to_datetime(
+        stop_times_df["departure_time"], format="%H:%M:%S"
+    )
+    stop_times_df["start_date"] = datetime.now()
+    stop_times_df["agency"] = agency
+
+    with db_adapter.engine.connect() as con:
+        stop_times_df.to_sql(
+            name="gtfs_stop_times",
+            con=con,
+            if_exists="append",
+            index=False,
+            method="multi",
+        )
+        con.commit()
+
+db_adapter.close()

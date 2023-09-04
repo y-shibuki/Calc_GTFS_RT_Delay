30ca561e38b83bff398908c271f3458c2fd1ca7d
Hinata Yoshida <12867630+y-shibuki@users.noreply.github.com>
2023-09-05 01:56:45 +0900

    gtfsを一括で更新するように変更

--- app/src/update_gtfs.py
+++ app/src/update_gtfs.py
@@ -7,7 +7,7 @@ from app.utils.db import get_db_adapter
 
 db_adapter = get_db_adapter()
 
-folder_path = "/Volumes/SSD/GTFS_DATA/gtfs_auto_downloader"
+folder_path = "/Volumes/SSD/GTFS_DATA/"
 
 # テーブルのデータを全て消去
 db_adapter.exec_query(
@@ -18,36 +18,38 @@ db_adapter.exec_query(
 
 for agency in ["関東自動車", "富山地鉄バス", "富山地鉄市内電車"]:
     for path in glob.glob(f"{folder_path}/gtfs/{agency}/*"):
-        print(path)
-    break
-    stop_times_df = pd.read_csv(f"./data/GTFS/{agency}/stop_times.txt")[
-        [
-            "trip_id",
-            "arrival_time",
-            "departure_time",
-            "stop_id",
-            "stop_sequence",
-            "stop_headsign",
+        start_date = path.split("/")[-1]
+
+        print(agency, start_date)
+
+        stop_times_df = pd.read_csv(f"{path}/stop_times.txt")[
+            [
+                "trip_id",
+                "arrival_time",
+                "departure_time",
+                "stop_id",
+                "stop_sequence",
+                "stop_headsign",
+            ]
         ]
-    ]
 
-    stop_times_df["arrival_time"] = pd.to_datetime(
-        stop_times_df["arrival_time"], format="%H:%M:%S"
-    )
-    stop_times_df["departure_time"] = pd.to_datetime(
-        stop_times_df["departure_time"], format="%H:%M:%S"
-    )
-    stop_times_df["start_date"] = datetime.now()
-    stop_times_df["agency"] = agency
-
-    with db_adapter.engine.connect() as con:
-        stop_times_df.to_sql(
-            name="gtfs_stop_times",
-            con=con,
-            if_exists="append",
-            index=False,
-            method="multi",
+        stop_times_df["arrival_time"] = pd.to_datetime(
+            stop_times_df["arrival_time"], format="%H:%M:%S"
         )
-        con.commit()
+        stop_times_df["departure_time"] = pd.to_datetime(
+            stop_times_df["departure_time"], format="%H:%M:%S"
+        )
+        stop_times_df["start_date"] = datetime.strptime(start_date, "%Y-%m-%d").date()
+        stop_times_df["agency"] = agency
+
+        with db_adapter.engine.connect() as con:
+            stop_times_df.to_sql(
+                name="gtfs_stop_times",
+                con=con,
+                if_exists="append",
+                index=False,
+                method="multi",
+            )
+            con.commit()
 
 db_adapter.close()

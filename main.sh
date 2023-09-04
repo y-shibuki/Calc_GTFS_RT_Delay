abee9252083c8d51e622fd13f32c83b3f87ddc92
Hinata Yoshida <12867630+y-shibuki@users.noreply.github.com>
2023-09-05 01:50:44 +0900

    バッシュコマンドの追加

---
+++ main.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+source ./.venv/bin/activate
+source ./.env.local
+
+export PYTHONPATH=/Users/shibuki/Documents/GTFS_delay/app:$PYTHONPATH
+
+if [ "$1" = "update_gtfs_rt" ]; then
+    python3 ./app/src/update_gtfs_rt.py $2
+elif [ "$1" = "update_gtfs" ]; then
+    python3 ./app/src/update_gtfs.py
+elif [ "$1" = "calc_delay" ]; then
+    python3 ./app/src/calc_delay.py
+else
+    echo "コマンドが不明です"
+fi
+
+deactivate
